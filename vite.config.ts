2d7c1c706fec9bb69b4feee5f3c84a829abd221d
Mohamed <mohamedabdelhakeem197@gmail.com>
2025-04-06 17:38:57 +0200

    Deployment Features

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: "/furnitures-eCommerce/",
+  plugins: [react(), tailwindcss()]
 });
