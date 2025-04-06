7b0928020a5dfcad30a60ecba3b3c7469b5c9e1c
Mohamed-Abdelhakeem1 <mohamedabdelhakeem197@gmail.com>
2025-04-06 20:14:13 +0200

    Editing imgages src

--- src/Components/Home/OffersBanner.tsx
+++ src/Components/Home/OffersBanner.tsx
@@ -1,10 +1,13 @@
+import firstImg from "/src/assets/Hero_banner/01.jpg";
+import secondImg from "/src/assets/Hero_banner/04.jpg";
+
 const OffersBanner = () => {
   return (
     <section className="py-6 ">
       <div className="contain flex-center flex-col sm:flex-row gap-2 sm:gap-4 lg:gap-8">
         <div className="relative w-full sm:flex-1">
           <div className="w-full">
-            <img className="w-full" src="/src/assets/Hero_banner/04.jpg"></img>
+            <img className="w-full" src={secondImg}></img>
           </div>
           <div className="absolute bottom-0 p-3">
             <h2 className="text-xl font-semibold">Limited Time Offers:</h2>
@@ -16,7 +19,7 @@ const OffersBanner = () => {
         <div className="relative w-full sm:flex-1 mt-2 sm:mt-0">
           <div className="w-full">
             {/* <img className="w-full" src="/src/assets/Hero_banner/01.jpg"></img> */}
-            <img className="w-full" src="../../assets/Hero_banner/01.jpg"></img>
+            <img className="w-full" src={firstImg}></img>
           </div>
           <div className="absolute top-0 p-3">
             <h2 className="text-xl font-semibold">Exclusive Deals:</h2>
