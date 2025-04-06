5d2484d98e4ff814bec518c09c9a7d20bd62debe
Mohamed-Abdelhakeem1 <mohamedabdelhakeem197@gmail.com>
2025-04-06 18:32:24 +0200

    try to deploy images

--- src/Components/Home/OffersBanner.tsx
+++ src/Components/Home/OffersBanner.tsx
@@ -16,11 +16,7 @@ const OffersBanner = () => {
         <div className="relative w-full sm:flex-1 mt-2 sm:mt-0">
           <div className="w-full">
             {/* <img className="w-full" src="/src/assets/Hero_banner/01.jpg"></img> */}
-            <img
-              className="w-full h-full object-cover"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPJ_V1FKOPLLXoULGd9LwmOkbXYjmeEBUeo-jcqvKV4LX2SoWftoj46cVqGprD1YVr2Ug&usqp=CAU"
-            ></img>
-            {/* <img className="w-full" src="../../assets/Hero_banner/01.jpg"></img> */}
+            <img className="w-full" src="../../assets/Hero_banner/01.jpg"></img>
           </div>
           <div className="absolute top-0 p-3">
             <h2 className="text-xl font-semibold">Exclusive Deals:</h2>
